b8f852ff85b4ebfab8c319f0694a3977e297a5b7
Thomas Schroedter <Thomas.Schroedter@gmx.net>
2015-07-08 16:26:15 +1000

    Added TurnCircleArcSegment to Shapes

---
+++ Selkie.Geometry/Shapes/ITurnCircleArcSegment.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using Selkie.Geometry.Primitives;
+
+namespace Selkie.Geometry.Shapes
+{
+    public interface ITurnCircleArcSegment : IPolylineSegment
+    {
+        [NotNull]
+        IArcSegment ArcSegment { get; }
+
+        Constants.CircleOrigin CircleOrigin { get; }
+        bool IsUnknown { get; }
+        Angle Angle { get; }
+        Angle AngleClockwise { get; }
+        Angle AngleCounterClockwise { get; }
+        Constants.TurnDirection TurnDirection { get; }
+    }
+}
