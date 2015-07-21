738c65515411fc55cd07b40298713f89f0ad791d
Thomas Schroedter <Thomas.Schroedter@gmx.net>
2015-07-22 09:19:01 +1000

    Updated from private repository

---
+++ Selkie.Geometry/Shapes/ITurnCircleArcSegment.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+namespace Selkie.Geometry.Shapes
+{
+    public interface ITurnCircleArcSegment : IArcSegment
+    {
+        [NotNull]
+        IArcSegment ArcSegment { get; }
+        Constants.CircleOrigin CircleOrigin { get; }
+    }
+}
