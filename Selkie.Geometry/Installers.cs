ff12a985abefce2ad73d2545583ba994115aae83
Thomas Schroedter <Thomas.Schroedter@gmx.net>
2015-09-11 20:47:07 +1000

    Fixed Installer comiler error

--- Selkie.Geometry/Installers.cs
+++ Selkie.Geometry/Installers.cs
@@ -4,5 +4,9 @@ namespace Selkie.Geometry
 {
     public class Installer : BaseInstaller <Installer>
     {
+        public override string GetPrefixOfDllsToInstall()
+        {
+            return "Selkie.";
+        }
     }
 }
