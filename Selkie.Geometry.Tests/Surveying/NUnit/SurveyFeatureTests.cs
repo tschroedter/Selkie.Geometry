ac511caf75b4b4da39a065f03d9ecd802b0dbada
thomas <thomas.schroedter@gmx.net>
2016-05-03 12:07:10 +1000

    Added SurveyFeature.Unknown

---
+++ Selkie.Geometry.Tests/Surveying/NUnit/SurveyFeatureTests.cs
@@ -0,0 +1,100 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using Selkie.Geometry.Primitives;
+using Selkie.Geometry.Shapes;
+using Selkie.Geometry.Surveying;
+
+namespace Selkie.Geometry.Tests.Surveying.NUnit
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    internal sealed class SurveyFeatureTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            m_Sut = SurveyFeature.Unknown;
+        }
+
+        private SurveyFeature m_Sut;
+
+        [Test]
+        public void AngleToXAxisAtEndPoint_ReturnsUnknown_WhenCalled()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.AreEqual(Angle.Unknown,
+                            m_Sut.AngleToXAxisAtEndPoint);
+        }
+
+        [Test]
+        public void AngleToXAxisAtStartPoint_ReturnsUnknown_WhenCalled()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.AreEqual(Angle.Unknown,
+                            m_Sut.AngleToXAxisAtStartPoint);
+        }
+
+        [Test]
+        public void EndPoint_ReturnsUnknown_WhenCalled()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.AreEqual(Point.Unknown,
+                            m_Sut.EndPoint);
+        }
+
+        [Test]
+        public void Id_ReturnsUnknown_WhenCalled()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.AreEqual(SurveyFeature.UnknownId,
+                            m_Sut.Id);
+        }
+
+        [Test]
+        public void IsUnknown_ReturnsTrue_WhenCalled()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.True(m_Sut.IsUnknown);
+        }
+
+        [Test]
+        public void Length_ReturnsUnknown_WhenCalled()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.AreEqual(SurveyFeature.UnknownLength,
+                            m_Sut.Length);
+        }
+
+        [Test]
+        public void RunDirection_ReturnsUnknown_WhenCalled()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.AreEqual(Constants.LineDirection.Unknown,
+                            m_Sut.RunDirection);
+        }
+
+        [Test]
+        public void StartPoint_ReturnsUnknown_WhenCalled()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.AreEqual(Point.Unknown,
+                            m_Sut.StartPoint);
+        }
+    }
+}
---
+++ Selkie.Geometry/Surveying/SurveyFeature.cs
@@ -0,0 +1,33 @@
+﻿using Selkie.Geometry.Primitives;
+using Selkie.Geometry.Shapes;
+
+namespace Selkie.Geometry.Surveying
+{
+    public class SurveyFeature : ISurveyFeature
+    {
+        internal const int UnknownId = -1;
+        internal const double UnknownLength = double.MaxValue;
+        public static SurveyFeature Unknown = new SurveyFeature();
+
+        private SurveyFeature()
+        {
+            IsUnknown = true;
+            StartPoint = Point.Unknown;
+            EndPoint = Point.Unknown;
+            AngleToXAxisAtStartPoint = Angle.Unknown;
+            AngleToXAxisAtEndPoint = Angle.Unknown;
+            RunDirection = Constants.LineDirection.Unknown;
+            Id = UnknownId;
+            Length = UnknownLength;
+        }
+
+        public bool IsUnknown { get; private set; }
+        public Point StartPoint { get; private set; }
+        public Point EndPoint { get; private set; }
+        public Angle AngleToXAxisAtStartPoint { get; private set; }
+        public Angle AngleToXAxisAtEndPoint { get; private set; }
+        public Constants.LineDirection RunDirection { get; private set; }
+        public int Id { get; private set; }
+        public double Length { get; private set; }
+    }
+}
